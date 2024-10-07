2bc03d46b6e7d72b1d60ab41c7f113d43eea2cfd
Ahmed-Samirr <88982340+Ahmed-Samirr@users.noreply.github.com>
2024-10-07 21:42:21 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+require("dotenv").config();
+const connectDB = require('./db/connect')
+var mongoose = require('mongoose');
+const productsRouter = require('./routes/product.routes')
+const userRouter = require('./routes/user.routes')
+const cookieParser = require('cookie-parser')
+const path = require('node:path')
+const cors = require( 'cors' );
+const xss = require( 'xss-clean' );
+const helmet = require( 'helmet' );
+const { use } = require( 'bcrypt/promises' );
+
+
+//middlewares
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname,"")))
+
+app.use(cors());
+app.use(xss());
+app.use(helmet());
+
+//routes
+
+app.use('/api/v1/products', productsRouter)
+app.use( '/api/v1/users', userRouter )
+
+
+//connect to databaseexpress. and start server
+const start = ( async () =>
+{
+    const cors = require( 'cors' );
+    const xss = require( 'xss-clean' );
+    const helmet = require( 'helmet' );
+
+
+    
+    const port = process.env.PORT
+    const uri = process.env.MONGO_URI ;
+    try {
+        
+        //connect DB
+        await connectDB(uri)
+
+        //start server
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
+        
+    } catch (error) {
+        console.log(error)
+    }
+
+})()
